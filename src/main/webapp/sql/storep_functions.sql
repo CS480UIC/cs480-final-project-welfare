3efdbb1fb112bd38683fd1aa125505b3872d0770
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2021-11-11 01:17:39 -0600

    functions and procedures

---
+++ src/main/webapp/sql/storep_functions.sql
@@ -0,0 +1,46 @@
+### SELECT ALL RECIPIENTS PROCEDURE ###
+
+USE `welfare`;
+DROP procedure IF EXISTS `selectAllRecips`;
+
+USE `welfare`;
+DROP procedure IF EXISTS `welfare`.`selectAllRecips`;
+;
+
+DELIMITER $$
+USE `welfare`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `selectAllRecips`()
+BEGIN
+SELECT * FROM recipient;
+END$$
+
+DELIMITER ;
+;
+
+#### GET RECIPIENT INCOME FUNCTION ###
+
+USE `welfare`;
+DROP function IF EXISTS `getRecipIncome`;
+
+USE `welfare`;
+DROP function IF EXISTS `welfare`.`getRecipIncome`;
+;
+
+DELIMITER $$
+USE `welfare`$$
+CREATE DEFINER=`root`@`localhost` FUNCTION `getRecipIncome`(firstName VARCHAR(60), lastName VARCHAR(60)) RETURNS int
+    READS SQL DATA
+BEGIN
+DECLARE recipID INT;
+DECLARE recipIncome INT;
+SELECT ID INTO recipID
+FROM recipient
+WHERE first_name = firstName AND last_name = lastName;
+SELECT total_net INTO recipIncome
+FROM income
+WHERE recipient_ID = recipID;
+RETURN recipIncome;
+END$$
+
+DELIMITER ;
+;
