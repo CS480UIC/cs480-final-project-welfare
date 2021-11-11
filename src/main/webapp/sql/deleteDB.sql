1e934b30813761eac470e241628b5882f3f9c56c
Harshal Patel <harshalpatel165@gmail.com>
2021-11-10 23:24:48 -0600

    deleteDB.sql file

---
+++ src/main/webapp/sql/deleteDB.sql
@@ -0,0 +1,13 @@
+DROP TABLE login;
+DROP TABLE entity1;
+DROP TABLE user;
+DROP TABLE recipient;
+DROP TABLE administrator;
+DROP TABLE address;
+DROP TABLE income;
+DROP TABLE eligibility;
+DROP TABLE housing;
+DROP TABLE medical;
+DROP TABLE nutritional;
+DROP TABLE medicalfacility;
+DROP TABLE nutritionalsource;
