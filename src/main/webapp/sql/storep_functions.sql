51f58c4bc538b3bd650f02dc53e99b590b7cd066
Harshal Patel <harshalpatel165@gmail.com>
2021-11-11 22:26:00 -0600

    storep and function - Harshal Patel

--- src/main/webapp/sql/storep_functions.sql
+++ src/main/webapp/sql/storep_functions.sql
@@ -44,3 +44,37 @@ END$$
 
 DELIMITER ;
 ;
+### SELECT ALL ADDRESS PROCEDURE ###
+USE `welfare`;
+DROP procedure IF EXISTS `new_procedure`;
+
+DELIMITER $$
+USE `welfare`$$
+CREATE PROCEDURE `new_procedure` ()
+BEGIN
+SELECT recipient_ID
+FROM address
+WHERE city = "Chicago";
+END$$
+
+DELIMITER ;
+;
+#### GET TOTAL NET INCOME OF ALL RECIPIENTS FUNCTION ###
+
+USE welfare;
+DROP function IF EXISTS new_function;
+
+DELIMITER $$
+USE welfare$$
+CREATE FUNCTION new_function ()
+RETURNS INTEGER
+READS SQL DATA
+BEGIN
+DECLARE netSum INT;
+SELECT SUM(total_net) INTO netSum
+FROM income;
+RETURN netSum;
+END$$
+
+DELIMITER ;
+;
